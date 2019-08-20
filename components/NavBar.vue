5ee294f070a19b60e9a07e8a259639eed6553b76
wa12rior <kamilserafindev@gmail.com>
2019-08-20 23:20:49 +0200

    Create and launch nuxt app, add some routes

---
+++ components/NavBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="nav">
+    <nuxt-link to="/" class="brand">Real World Events</nuxt-link>
+    <nav>
+      <nuxt-link to="/" class="brand">Links</nuxt-link> |
+      <nuxt-link to="/create" class="brand">Create</nuxt-link>
+    </nav>
+  </div>
+</template>
